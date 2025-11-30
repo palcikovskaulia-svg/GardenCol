f5002ccc0c927aab709acec1deb22b01a09f2038
Yuliia <Palcikovskaulia@gmail.com>
2025-11-30 17:43:17 +0200

    fix: Corrected dj_database_url argument to 'conn_health_checks'

--- GardenCol_Project/settings.py
+++ GardenCol_Project/settings.py
@@ -83,7 +83,7 @@ if os.environ.get('DATABASE_URL'):
         'default': dj_database_url.config(
             default=os.environ.get('DATABASE_URL'),
             conn_max_age=600,
-            conn_health_check=True,
+            conn_health_checks=True,
         )
     }
 else:
